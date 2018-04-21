fc3454837881dfbe2f2bd390ae3fc4858f2d4b92
matthewsommer <matthew.sommer@gmail.com>
2018-04-21 12:20:15 -0700

    Fixed bad import

--- app/src/androidTest/java/io/mattsommer/ui/movie/MovieListActivityTest.java
+++ app/src/androidTest/java/io/mattsommer/ui/movie/MovieListActivityTest.java
@@ -10,24 +10,20 @@ import static junit.framework.Assert.assertEquals;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.util.Log;
-import android.widget.TextView;
-import android.widget.Toolbar;
-import io.mattsommer.popularmovies.R;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
- * Instrumented tests for the default activity
- * Created by matt on 10/15/17.
+ * Instrumented tests for the default activity Created by matt on 10/15/17.
  */
 @RunWith(AndroidJUnit4.class)
 @LargeTest
 public class MovieListActivityTest {
 
   @Rule
-  public ActivityTestRule<MovieListActivity> mActivityRule = new ActivityTestRule<>(MovieListActivity.class);
+  public ActivityTestRule<MovieListActivity> mActivityRule = new ActivityTestRule<>(
+      MovieListActivity.class);
 
   @Test
   public void views() {
